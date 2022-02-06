e3385616ed5ddbaa032474c3b0c54f442fbe8fad
KT-555 <tucker2009@gmail.com>
2022-02-06 16:21:44 -0330

    Initial commit

---
+++ app/src/main/java/com/example/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder(val binding:
+        ListSelectionViewHolderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        }
